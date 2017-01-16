e510d3896b08bcaf2a2f0e5fa21b7a9de6f56d19
kevinbehan <kevin@ucode.com>
2017-01-16 02:37:10 +0000

    added 'enter to send message' with checkbox option.  Added main element to contain message articles.  moved form to the bottom of the page

--- message-ui.js
+++ message-ui.js
@@ -36,16 +36,30 @@ msgForm.addEventListener("submit", setMsgUI)
 
 function clearForm(evt) {
     evt.preventDefault()
-    document.querySelector("textarea").value = ""
-    document.querySelector("input").value = ""
+    msgForm.reset()
     document.querySelector("textarea").focus()
 }
 
+
+
 msgForm.addEventListener("submit", clearForm)
 
-/*Whenever you complet a feature:
+/*Whenever you complete a feature:
 1. cd into message-app
 2. in the command line put git add --all
 3. Then git commit -m "Put a descriptive message about your app here"
 4. Then git push
 */
+
+//problem: add a checkbox with an event listener that, when unchecked, calls setMsgUi
+function sendOnEnter(event){
+    if(event.which === 13)
+        msgFor.submit()
+}
+
+
+$("[name='enter-to-send']").change(function(){
+     $(this).keypress(sendOnEnter)
+})
+//add event listener 
+
